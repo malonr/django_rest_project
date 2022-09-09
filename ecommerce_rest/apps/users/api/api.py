e5c65965fbb584ec4e9e97b4abbd20017cf66ea4
malonr <marlon.ramirezlizcano@gmail.com>
2022-09-09 10:23:05 -0500

    feat(api-users): add GET, PUT, DELETE method

--- ecommerce_rest/apps/users/api/api.py
+++ ecommerce_rest/apps/users/api/api.py
@@ -1,5 +1,3 @@
-from http.client import ResponseNotReady
-from urllib import request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view 
@@ -20,3 +18,23 @@ def user_api_view(request):
             user_serializer.save()
             return Response(user_serializer.data)
         return Response(user_serializer.errors) 
+
+@api_view(['GET','PUT','DELETE'])
+def user_detail_api_view(request, pk=None):
+    if request.method == 'GET':
+        user = User.objects.filter(id = pk).first()
+        user_serializer = UserSerializer(user)
+        return Response(user_serializer.data)
+
+    elif request.method == 'PUT':
+        user = User.objects.filter(id = pk).first()
+        user_serializer = UserSerializer(user, data = request.data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response(user_serializer.data)
+        return Response(user_serializer.errors)
+    
+    elif request.method == 'DELETE':
+        user = User.objects.filter(id = pk).first()
+        user.delete()
+        return Response('Eliminated')
